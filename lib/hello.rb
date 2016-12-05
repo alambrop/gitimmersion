783382fc526083294f8408aa3b146cbd7e2675d7
alambrop <alambrop@uncc.edu>
2016-12-05 01:29:22 +0000

    Added greeter class

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 # Author: Andrew Lambropoulos (alambrop@uncc.edu)
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet 
